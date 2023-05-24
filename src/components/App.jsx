900a0b38d6bddc6e0ceebb7ba23e49e6ea1dcbbe
anastasia-demia <109302069+anastasia-demia@users.noreply.github.com>
2023-05-24 15:05:35 +0300

    sction and btn done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from 'react';
+import { Section } from './Section/Section';
+import { Button } from './Button/Button';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: "",
+  };
+
+
+
+  render() {
+
+
+    return(
+      <>
+      <Section
+      title="Phonebook">
+        <Button text="Add Contact"/>
+      </Section>
+      <Section
+      title="Find Contacts By Name"
+      >
+        <Button text="Find"/>
+      </Section>
+      <Section
+      title="Contacts"
+      >
+        <Button text="Delete"/>
+      </Section>
+      </>
+    )
+  }
 };
